76c55fea012d2d42b83febfc919dc89a92857137
nayeem01 <nayeemabdullah00@gmail.com>
2022-05-07 11:56:33 +0600

    mask detection done

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-### facemaske detection from scratch
+### Real time facemask detection
 
+-   Dataset: https://www.kaggle.com/datasets/omkargurav/face-mask-dataset
+
+-   Project Architecture  
+    ![Project Architecture ](/assets/faceMask-Architecture.jpg "Project Architecture")
+-   Demo
+    ![Alt Text](/assets/demo.gif)
+
+-   how to run this project
+    -   python3 -m venv env
+    -   source env/bin/activate
+    -   pip install -r requirements.txt
+    -   python <strong>script.py</strong>
--- script.py
+++ script.py
@@ -1,17 +1,29 @@
 import cv2 as cv
+from matplotlib.pyplot import text
 import tensorflow as tf
 from numba import cuda
+import numpy as np
+from keras.models import model_from_json
+# import math
 
+# resetting GPU
 device = cuda.get_current_device()
 device.reset()
 
+# configuring GPU
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
 
-# tf.compat.v1.keras.backend.set_session(session)
 
-model = tf.keras.models.load_model('CNN_64X3.h5')
+className = {0: "no mask", 1: "masked"}
+
+
+json_file = open('models/mask_model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+mask_model = model_from_json(loaded_model_json)
+mask_model.load_weights("models/CNN_64X3.h5")
 
 
 def rescaleFrame(frame, scale=0.75):
@@ -23,29 +35,69 @@ def rescaleFrame(frame, scale=0.75):
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 
-def prepare(filepath):
-    IMG_SIZE = 65  # 50 in txt-based
-    img_array = cv.imread(filepath, cv.IMREAD_GRAYSCALE)
-    new_array = cv.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+def process_input(img):
+    IMG_SIZE = 69
+    # gry_img = cv.imread(img, cv.IMREAD_GRAYSCALE)
+    img = cv.resize(img, (IMG_SIZE, IMG_SIZE))
+    return img.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 
-# REMEMBER YOU'RE PASSING A LIST OF THINGS YOU WISH TO PREDICT
-prediction = model.predict([prepare('test.jpg')])
+# def detected_mask():
+#     # model = tf.keras.models.load_model('CNN_64X3.h5')
+#     prediction = mask_model.predict([process_input('test/test.jpg')])
+#     maxindex = int(prediction[0][0])
+#     print(math.ceil(prediction[0][0]))
+#     print(np.argmax(prediction))
+#     print(maxindex)
+#     print(className[maxindex])
 
-print(prediction[0][0])
+
+# detected_mask()
+font = cv.FONT_HERSHEY_SIMPLEX
+capture = cv.VideoCapture('test/test2.mp4')
+# for web cam
 # capture = cv.VideoCapture(0)
 
-# while True:
-#     isTrue, frame = capture.read()
+while True:
+    isTrue, frame = capture.read()
 
-#     frame_resized = rescaleFrame(frame, scale=.2)
+    face_detector = cv.CascadeClassifier(
+        'models/haarcascade_frontalface_default.xml')
 
-#     #cv.imshow('Video', frame)
-#     cv.imshow('Video Resized', frame_resized)
+    frame = rescaleFrame(frame, 0.3)
 
-#     if cv.waitKey(20) & 0xFF == ord('d'):
-#         break
+    gray_face = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-# capture.release()
-# cv.destroyAllWindows()
+    faces = face_detector.detectMultiScale(
+        gray_face, scaleFactor=1.09, minNeighbors=7,  minSize=(30, 30), flags=cv.CASCADE_SCALE_IMAGE)
+
+    # print(faces)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y-50), (x+w, y+h+10), (0, 255, 0), 4)
+        rec_frame = gray_face[y:y + h, x:x + w]
+        cropped_img = process_input(rec_frame)
+
+        prediction = mask_model.predict([cropped_img])
+        maxindex = round(prediction[0][0])
+
+        str_classname = "{} {}".format(
+            className[maxindex], "{:.2f}".format(prediction[0][0], 2))
+        str_facecount = "face count : {}".format(len(faces))
+
+        cv.putText(frame, str_classname, (x+5, y-20),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, (15, 15, 253) if maxindex == 0 else (0, 255, 0), 2, cv.LINE_AA)
+
+        cv.putText(frame, str_facecount,
+                   (40, 40),  font, 1, (255, 0, 0), 2, cv.LINE_AA)
+        #frame_resized = rescaleFrame(frame, scale=.2)
+
+        cv.imshow('Mask-detection', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('d'):
+        break
+    elif 0xFF == ord('q'):
+        cv.waitKey(1000)
+
+capture.release()
+cv.destroyAllWindows()
